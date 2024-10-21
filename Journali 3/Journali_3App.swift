b3d600571c7eca78432999370c6594f5f4920386
reem <reemhmood231@gmail.com>
2024-10-21 10:02:41 +0300

    Initial Commit

---
+++ Journali 3/Journali_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Journali_3App.swift
+//  Journali 3
+//
+//  Created by REEM alghafes on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Journali_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Journali 3Tests/Journali_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Journali_3Tests.swift
+//  Journali 3Tests
+//
+//  Created by REEM alghafes on 18/04/1446 AH.
+//
+
+import Testing
+@testable import Journali_3
+
+struct Journali_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

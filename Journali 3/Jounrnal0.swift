a5b4163d0a828d0884f822f1924b3c73bb514b94
reem <reemhmood231@gmail.com>
2024-10-29 09:45:46 +0300

    SwiftData

---
+++ Journali 3/Jounrnal0.swift
@@ -0,0 +1,8 @@
+//
+//  Jounrnal0.swift
+//  Journali 3
+//
+//  Created by REEM alghafes on 25/04/1446 AH.
+//
+
+import Foundation
--- Journali 3/MainPage3.swift
+++ Journali 3/MainPage3.swift
@@ -1,18 +1,18 @@
 //
-//  MainPage2.swift
+//  MainPage3.swift
 //  Journali 3
 //
-//  Created by REEM alghafes on 18/04/1446 AH.
+//  Created by REEM alghafes on 25/04/1446 AH.
 //
 
 import SwiftUI
 
-struct MainPage2: View {
+struct MainPage3: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    MainPage2()
+    MainPage3()
 }
